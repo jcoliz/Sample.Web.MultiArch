35dc0f779ac6d8adb3b3479b3d1970ead05e50f0
James Coliz <jcoliz@outlook.com>
2024-04-05 14:38:48 -0700

    build(test): dotnet new nunit -f net8.0 -o Tests.Unit -n Sample.Web.MultiArch.Tests.Unit

---
+++ Tests.Unit/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Sample.Web.MultiArch.Tests.Unit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
